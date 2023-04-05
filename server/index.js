5c351f474f34bec59dcf7a280667c0d7a93604a2
Akash Mukherjee <mukherjeeakash009@gmail.com>
2023-04-05 15:42:41 +0530

    crud

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app= express()
 app.use(cors())
 app.use(express.json())
 
-//routes
+//apps
 
 //create
 app.post('/todos', async (req,res)=>{
@@ -62,6 +62,72 @@ app.delete('/todos/:id',async (req,res)=>{
         console.error(error.message)
     }
 })
+
+app.post("/register", async (req, res) => {
+    const { email, password } = req.body;
+  
+    try {
+      const user = await pool.query("SELECT * FROM user_details WHERE user_email = $1", [
+        email
+      ]);
+  
+      if (user.rows.length > 0) {
+        return res.status(401).json("User already exist!");
+      }
+  
+      const salt = await bcrypt.genSalt(10);
+      const bcryptPassword = await bcrypt.hash(password, salt);
+  
+      let newUser = await pool.query(
+        "INSERT INTO user_details (user_email, user_password) VALUES ($1, $2) RETURNING *",
+        [email, bcryptPassword]
+      );
+  
+      const jwtToken = jwtGenerator(newUser.rows[0].user_id);
+  
+      return res.json({ jwtToken });
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  });
+  
+  app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+  
+    try {
+      const user = await pool.query("SELECT * FROM user_details WHERE user_email = $1", [
+        email
+      ]);
+  
+      if (user.rows.length === 0) {
+        return res.status(401).json("Invalid Credential");
+      }
+  
+      const validPassword = await bcrypt.compare(
+        password,
+        user.rows[0].user_password
+      );
+  
+      if (!validPassword) {
+        return res.status(401).json("Invalid Credential");
+      }
+      const jwtToken = jwtGenerator(user.rows[0].user_id);
+      return res.json({ jwtToken });
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  });
+  
+  app.post("/verify", (req, res) => {
+    try {
+      res.json(true);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  });
 
 
 
